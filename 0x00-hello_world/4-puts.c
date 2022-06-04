c0780b43cfa10318bfa0245213b94bb095d23661
deenlennon <github.com/deenlennon>
2022-06-04 13:47:51 -0700

    4.Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
-#!/bin/bash
-/*
- * File: 4-puts.c 
- * Auth: Deen Lennon <@udayomo.com>
- */
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual
- * 		puzzle, followed by a new line.
- *
- * 		Return: Always 0.
- */
+ *  *  *main - Entry point
+ *  *  *
+ *  *  *Return: Always 0 (Success)
+ *  	**/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
