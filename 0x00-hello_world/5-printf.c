912e0ec45261b5e6f3b2e711c3a402a97b81c983
deenlennon <github.com/deenlennon>
2022-06-05 14:38:56 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Description: Runs printf and print statement
  * Return: Return 0 to exit
  */
-int main(void) 
+int main(void)
 {
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
