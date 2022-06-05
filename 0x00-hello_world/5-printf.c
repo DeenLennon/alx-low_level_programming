8d6a71b4aa40deb6cf93d8fbaa9d868e4cc4a4b1
deenlennon <github.com/deenlennon>
2022-06-05 14:24:14 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main - run the main function
+ * Description: Runs printf and print statement
+ * Return: Return 0 to exit
+ */
 int main(void) 
 {
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
