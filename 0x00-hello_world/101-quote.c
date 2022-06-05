ef9cc714e913c3ba3f9f7960ff29e4279fc89555
deenlennon <github.com/deenlennon>
2022-06-05 16:42:00 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,6 @@
  */
 int main(void)
 {
-	int l;
-
-	l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+	write(1, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
