1055cda9527083ce6aab9fa0a232d483d1aa31d8
deenlennon <github.com/deenlennon>
2022-06-06 02:58:34 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	last_digit = n % 10;
-	printf("Last digit of 98 is 8 and is greater than 5 ", n = 98);
+	printf("Last digit of 98 is 8 and is greater than 5\n ", n = 98);
 
 	if (last_digit > 5)
 	{
