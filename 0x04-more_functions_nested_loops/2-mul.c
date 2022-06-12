4e541893f7b6e994c0e40769b63b789c17014fc1
deenlennon <github.com/deenlennon>
2022-06-12 03:02:16 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply 2 number
+ * @a: integer
+ * @b: integer
+ * Return: Success integer.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 A function that checks for uppercase character
 A function that multiply two integers
+A function that multiply two integers
