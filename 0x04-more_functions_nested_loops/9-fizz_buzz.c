4c2020651c27756b6ca15822c167369f8a1fed22
deenlennon <github.com/deenlennon>
2022-06-14 07:06:57 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -11,7 +10,7 @@ void fizz_buzz(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if (x == 100)
-			printf("Buzz");
+			printf("Buzz ");
 		else if (x % 3 == 0 && x % 5 == 0)
 			printf("FizzBuzz ");
 		else if (x % 3 == 0)
