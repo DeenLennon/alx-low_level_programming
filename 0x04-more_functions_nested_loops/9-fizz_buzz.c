f485d62216aeef0e903797c71624ead80f9ae801
deenlennon <github.com/deenlennon>
2022-06-15 09:45:36 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * fizz_buzz - print Fizz Buzz
+ * main - Fizz Buzz
  *
  * Return: void
  */
@@ -13,19 +13,19 @@ int main(void)
 	{
 		if (((x % 3) == 0) && ((x % 5) == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if ((x % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if ((x % 5) == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%x", x);
+			printf("%x ", x);
 		}
 		if (x < 100)
 			printf(" ");
