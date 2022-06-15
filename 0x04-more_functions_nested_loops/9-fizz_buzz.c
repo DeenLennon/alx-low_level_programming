c0f6d9af4873f43fbd48408b92793b3d3f60d147
deenlennon <github.com/deenlennon>
2022-06-15 12:10:32 -0700

    hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * fizz_buzz - Fizz Buzz
@@ -13,22 +13,22 @@ void fizz_buzz(void)
 	{
 		if (((x % 3) == 0) && ((x % 5) == 0))
 		{
-			printf("FizzBuzz");
+			_putchar("FizzBuzz");
 		}
 		else if ((x % 3) == 0)
 		{
-			printf("Fizz");
+			_putchar("Fizz");
 		}
 		else if ((x % 5) == 0)
 		{
-			printf("Buzz");
+			_putchar("Buzz");
 		}
 		else
 		{
-			printf("%x", x);
+			_putchar("%x", x);
 		}
 		if (x < 100)
-			printf(" ");
+			_putchar(" ");
 	}
-	printf("\n");
+	_putchar("\n");
 }
