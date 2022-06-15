0f57d53d0333b1faf701d679408a87f56d125df2
deenlennon <github.com/deenlennon>
2022-06-15 14:06:41 -0700

    100-print_number.c

---
+++ 0x04-more_functions_nested_loops/100-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - a function that print integers
+ * @n: the integer
+ */
+void print_number(int n)
+{
+	unsigned long int a, i;
+
+	a = 612852475143;
+	for (i = 3; i < (a / 2); i = i + 2)
+	{
+		while ((a % i == 0) && (a != i))
+			a = a / i;
+	}
+	printf("%lu\n", a);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -9,3 +9,4 @@ A function that draws a diagonal line on the terminal
 A function that print a square followed by a new line
 Fizz-Buzz
 A function that prints a triangle followed by a new line
+A function that prints integer
