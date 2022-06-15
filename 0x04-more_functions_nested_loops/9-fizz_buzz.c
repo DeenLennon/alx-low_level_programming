733eb2424265806e87d93fa52b445c88300b82cd
deenlennon <github.com/deenlennon>
2022-06-15 13:10:06 -0700

    hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (((x % 3) == 0) && ((x % 5) == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if ((x % 3) == 0)
+		if ((x % 3) == 0)
 		{
 			printf("Fizz");
 		}
@@ -23,13 +19,15 @@ int main(void)
 		{
 			printf("Buzz");
 		}
+		else if (((x % 3) == 0) && ((x % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
 		else
 		{
 			printf("%x", x);
 		}
-		if (x < 100)
-			putchar(' ');
+		putchar(' ');
 	}
 	putchar('\n');
-	return (0);
 }
