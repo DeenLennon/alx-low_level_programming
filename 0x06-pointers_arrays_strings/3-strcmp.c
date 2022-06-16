26d95e3e176b27a7d8634f2e1e951d00f017a47f
deenlennon <github.com/deenlennon>
2022-06-16 14:13:31 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - A function that compares two strings
+ * @s1: first character
+ * @s2: second character
+ * Return: string
+ */
+int _strcmp(char *s1, char *s2);
+{
+	strcmp(s1, s2);
+	return (dest);
+}
