7da9a470291059a10446947b2f65d43c4fdc73c4
deenlennon <github.com/deenlennon>
2022-06-16 16:21:33 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - A function that capitalizes string letter
+ * @x: the variable to be capitalized
+ * Return: capitalized x
+ */
+char *cap_string(char *x)
+{	x = "lennon,is;in alx program africa"
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
+	int len = 13;
+	int a = 0, i;
+
+	while (x[i])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] = s[a] - 32;
+			i++;
+		}
+		a++
+	}
+	return (x);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@ A function that copies a string
 A function that compares two strings
 A function that reverses the content of an array of integer
 A function that changes the lower letter of a string to uppercase
+A function that capitalizes all words of a string
