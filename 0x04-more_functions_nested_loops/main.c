4ccbefd5fa1fc368a0526e1384478c1997adb9b8
deenlennon <github.com/deenlennon>
2022-06-17 09:40:19 -0700

    hot fix

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 
-int strlen(char *);
-
-/**
- * main - check the code for holberton school students
- *
- * Return: Always 0
- */
 int main(void)
 {
-	char *str;
-	int len;
-
-	str = "Holberton!";
-	len = _strlen(str);
-	printf("%d", len);
+	more_numbers();
 	return (0);
 }
