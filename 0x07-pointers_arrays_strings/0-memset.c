131c16e7959397cc3f3b8d466c7443ae206bbe2c
deenlennon <github.com/deenlennon>
2022-06-20 19:37:47 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - A function that fills memory with bytes
+ * @s: string
+ * @b: a character
+ * @n: an integer
+ * Return: a string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		s[x] = b;
+
+	return (s);
+}
