e4f54c07dc21731487b8fb7ddf5b907b40b6661f
deenlennon <github.com/deenlennon>
2022-06-23 12:35:17 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,8 +13,9 @@ int _evaluate(int i, int n)
 		return (n);
 	else if (i * i < n)
 		return (_evaluate(i + 1, n));
-	else if (i * 1 == n)
+	else if (i * i == n)
 		return (i);
+
 	return (-1);
 
 	return (-1);
