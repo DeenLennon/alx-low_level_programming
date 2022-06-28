03e4cbb187bc7e70f6a01ada42ae107769c60ea3
deenlennon <github.com/deenlennon>
2022-06-27 20:44:07 -0700

    hot fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,8 +9,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int index;
 
+	if (size == 0)
+		return (NULL);
 
-	return (0);
+	array = malloc(sizeof(char) * size);
 
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
 }
