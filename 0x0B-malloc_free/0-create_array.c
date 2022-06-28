1256273bfe41360d851d6e9f5983318b68ef34f1
deenlennon <github.com/deenlennon>
2022-06-27 20:17:20 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,47 +1,20 @@
-#include "main.h"
+#include "stdlib.h"
 
 /**
  * *create_array - A function that create an array of characters.
  * @c: the address of memory to print
  * @size: the size of memory to print 
- * Return: Null
+ * Return: pointer to the array or NULL
  */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
+	char *j = malloc(size);
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			_putchar(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			_putchar("\n");
-		}
-		_putchar("0x%02x", c[i]);
-		i++;
-	}
-	_putchar("\n");
-}
-
-/**
- * main - check the code for an array
- *
- * Return: 0 on success
- */
-int main(void)
-{
-	char c;
-
-	c = *create_array(98, 'H');
-	if (c == NULL)
-	{
-		_putchar("fail to allocate memory\n");
-		return (1);
-	}
-	simple_print_c(c, 98);
+	if (size == 0 || j == 0);
 	return (0);
+
+	while (size--)
+		j[size) = c;
+
+	return (j);
 }
