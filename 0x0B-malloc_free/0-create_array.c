c941631ea26013b7bffda837a47dbd635f1beec2
deenlennon <github.com/deenlennon>
2022-06-29 12:38:12 -0700

    hot fixt

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,8 +4,8 @@
 
 /**
  * *create_array - A function that create an array of characters.
- * @c: the address of memory to print
- * @size: the size of memory to print
+ * @size: input size of array
+ * @c: the initialized char
  * Return: pointer to the array or NULL
  */
 char *create_array(unsigned int size, char c)
