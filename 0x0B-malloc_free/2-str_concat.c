b84351b1420de99eda0c30f823ba9ad47d1bfa3e
DeenLennon <github.com/deenlennon>
2022-07-03 09:34:08 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,31 +9,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, x, y, z;
-	char *p;
+	int i = 0, j = 0, l = 0, k = 0;
+	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
 
-	for (i = 0; s1[i] != '\0'; i++)
-
-		for (j = 0; s2[j] != '\0'; j++)
-
-			p = malloc((i + j) * sizeof(char) + 1);
-
-	if (p == NULL)
-		return (NULL);
-
-	for (x = 0; x < i; x++)
-		p[x] = s1[x];
-
-	for (y = i, z = 0; y < i + j; y++, z++)
-		p[y] = s2[z];
-
-	p[i + j] = '\0';
-
-	return (p);
+	l = i + j;
+	s = (char *)malloc(l * sizeof(char) + 1);
+	j = 0;
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+		if (k > i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
 }
