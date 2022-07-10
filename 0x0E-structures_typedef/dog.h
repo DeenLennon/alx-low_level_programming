fb45cc5fb91e23a9179dcb70fa515e3f3753fdb5
DeenLennon <github.com/deenlennon>
2022-07-10 09:13:21 -0700

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,11 @@
 #define DOG_H
 
 /**
- * struct dog - A new type describing a dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner:  owner
+ * Description: This struct is for dog
  */
 
 struct dog
@@ -19,6 +20,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dot_t *d);
+void free_dog(dog_t *d);
 
 #endif /*DOG_H*/
